b9af80d514f8363329f29dfc637a894525fd45aa
RAHUL PANDA <rahul.panda@aptusdatalabs.com>
2019-03-10 18:57:08 +0530

    Lodash Module Implementation

---
+++ typescript_lodash/array_reversal.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var lodash = require("lodash");
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log("Original Array: " + array);
+console.log("Reveresed Arrag: " + lodash.reverse(array));
---
+++ typescript_lodash/array_reversal.ts
@@ -0,0 +1,4 @@
+import * as lodash from 'lodash';
+var array = [1,2,3,4,5,6,7,8,9];
+console.log("Original Array: "+array);
+console.log("Reveresed Arrag: "+lodash.reverse(array)); 
